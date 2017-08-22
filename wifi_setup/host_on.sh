898e0cde263a2b5020d5034c00db602594c858d7
Jony <liu.z.jonathan@gmail.com>
2017-08-21 19:31:33 -0700

    Fixed host_on
    Just in case wpa_cli is already terminated

--- wifi_setup/host_on.sh
+++ wifi_setup/host_on.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #string version:
 #"/usr/bin/python /usr/bin/wifi_configurator.py\necho \"Host is now on.\""
+wpa_cli ter
+killall hostapd
+wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
 /usr/bin/python /usr/bin/wifi_configurator.py
 echo "Host is now on."
